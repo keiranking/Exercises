86ff817e9f2c6121c6621406d0e469468389209d
keiranking <me@keiranking.com>
2018-04-05 17:01:55 -0700

    Node class.

--- linked_list.js
+++ linked_list.js
@@ -1,1 +1,14 @@
 // Implementing a linked list
+class Node {
+  constructor(data = null) {
+    this.data = data;
+    this.next = null;
+    // this.previous = null;
+  }
+
+}
+
+n1 = new Node("hello");
+n2 = new Node("world");
+n1.next = n2;
+console.log(n1.data, n1.next.data);
