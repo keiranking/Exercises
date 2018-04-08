4d76baed69617c7f0fa84991983e4dfd09503235
keiranking <me@keiranking.com>
2018-04-08 12:58:44 -0700

    Add insert method.

--- linked_list.js
+++ linked_list.js
@@ -5,10 +5,24 @@ class Node {
     this.next = null;
     // this.previous = null;
   }
+}
 
+class LinkedList {
+  constructor() {
+    this.head = null;
+    // this.tail = null;
+  }
+
+  insert(node) {
+    node.next = this.head;
+    this.head = node;
+  }
 }
 
 n1 = new Node("hello");
 n2 = new Node("world");
+l = new LinkedList();
+l.insert(new Node("world"));
+l.insert(new Node("Hello"));
 n1.next = n2;
-console.log(n1.data, n1.next.data);
+console.log(n1.data, n1.next.data, JSON.stringify(l));
