d0bd4f4ec04d113846c8ad2f7e6fbd96b135de55
keiranking <me@keiranking.com>
2018-04-08 15:51:01 -0700

    Add delete, display methods.

--- linked_list.js
+++ linked_list.js
@@ -17,12 +17,50 @@ class LinkedList {
     node.next = this.head;
     this.head = node;
   }
+
+  del() {
+    this.head = this.head.next;
+  }
+
+  display() {
+    this.print(this.head);
+  }
+
+  print(node) { // recursive helper function for display()
+    if (node) { // test for empty list
+      console.log(node.data);
+      if (node.next) { // test for end of list
+        this.print(node.next);
+      }
+    }
+  }
+
+  search(data) {
+    return this.find(data, this.head);
+  }
+
+  find(data, node) { // recursive helper function for search()
+    console.log("A", data, node.data);
+    if (node) {
+      console.log("B", data, node.data);
+      if (node.data == data) {
+        console.log("C", data, node.data);
+        return true;
+      } else if (node.next) {
+        return find(data, node.next);
+      }
+    }
+    return false;
+  }
 }
 
-n1 = new Node("hello");
-n2 = new Node("world");
-l = new LinkedList();
+let l = new LinkedList();
 l.insert(new Node("world"));
 l.insert(new Node("Hello"));
-n1.next = n2;
-console.log(n1.data, n1.next.data, JSON.stringify(l));
+l.insert(new Node("This"));
+console.log(JSON.stringify(l));
+l.del();
+console.log(JSON.stringify(l));
+l.display();
+console.log(l.search("world"));
+console.log(l.search("globe"));
